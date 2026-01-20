35bb004fb73fd8621ab2c07b79c9d0ccadaf4bcb
bemery <bryan.emery@etu.unistra.fr>
2026-01-20 02:20:01 +0100

    regular grid

--- grid_manager.gd
+++ grid_manager.gd
@@ -18,15 +18,26 @@ extends Node3D
 
 @export var collider : CollisionShape3D
 @export var mouse_collision : MouseCollision
-
+@export var hovered_gameobject : PackedScene
+var instance_hovered_gameobject : MeshInstance3D
 var grid_data : Array[ArrayMesh]
-
+var grid_bool : Array[bool]
 func _ready() -> void:
 	grid.create_mesh(Mesh.PRIMITIVE_LINES,grid_size,cell_size)
 	camera.global_position = Vector3(grid_size*cell_size/2.0,5,grid_size*cell_size/2.0)
 	collider.shape.size = Vector3(grid_size*cell_size,0.1,grid_size*cell_size)
 	collider.global_position = Vector3(grid_size*cell_size/2.0,-0.05,grid_size*cell_size/2.0)
+	instance_hovered_gameobject = hovered_gameobject.instantiate()
+	instance_hovered_gameobject.scale*=cell_size
+	add_child(instance_hovered_gameobject)
 
 
 func _physics_process(delta: float) -> void:
-	print(mouse_collision.mouse_grid_position(camera))
+	var mouse3d = mouse_collision.mouse_grid_position(camera)
+	if(mouse3d.x >= 0 and mouse3d.z >= 0 and mouse3d.x <= grid_size*cell_size and mouse3d.z <= grid_size*cell_size):
+		instance_hovered_gameobject.position = mouse3d
+		instance_hovered_gameobject.get_active_material(0).albedo_color = Color(1,1,1,1)
+	else :
+		instance_hovered_gameobject.get_active_material(0).albedo_color = Color(1,1,1,0)
+	m
+		
--- mouse_collision.gd
+++ mouse_collision.gd
@@ -3,6 +3,6 @@ extends Node
 
 class_name MouseCollision
 
-func mouse_grid_position(camera : Camera3D):
+func mouse_grid_position(camera : Camera3D) ->Vector3:
 	return (camera.project_ray_origin(get_viewport().get_mouse_position())*Vector3(1,0,1)).floor()+Vector3(0.5,0,0.5)
 	
